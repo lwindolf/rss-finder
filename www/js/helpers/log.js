4433b567ae00a36f989f18c5656d88534d37f423
Lars Windolf <lars.windolf@gmx.de>
2026-03-02 19:59:35 +0100

    Safer simpler logging

--- www/js/helpers/log.js
+++ www/js/helpers/log.js
@@ -2,21 +2,25 @@
 
 /* Provides trace topic like filtering of console.log + console.warn
    First word of the msg string is considered the topic name and topics
-   can be enabled by setting window.app.debug['topic']=true */
+   can be enabled by setting window.debug['topic']=true or
+   window.debug['all']=true to see all log messages */
 
-const filteredLog = (msg, ...optionalParams) => {
-    // If it is not a string it is a data dump, log it directly and always
-    if (typeof msg !== 'string') {
-        window.originalConsoleLog(msg, ...optionalParams);
-        return;
-    }
+if(!window.originalConsoleLog) {
+    const filteredLog = (msg, ...optionalParams) => {
+        // If it is not a string it is a data dump, log it directly and always
+        if (typeof msg !== 'string') {
+            window.originalConsoleLog(msg, ...optionalParams);
+            return;
+        }
 
-    const topic = msg.split(' ')[0];
-    if (window?.app?.debug[topic] || window?.app?.debug['all'])
-        window.originalConsoleLog(msg, ...optionalParams);
-};
-window.originalConsoleLog = window.console.log;
-window.console.log = filteredLog;
-window.console.info = filteredLog;
-window.console.warn = filteredLog;
-window.console.debug = filteredLog;
+        const topic = msg.split(' ')[0];
+        if (window?.debug?.[topic] || window?.debug?.['all'])
+            window.originalConsoleLog(msg, ...optionalParams);
+    };
+    window.originalConsoleLog = window.console.log;
+    window.console.log = filteredLog;
+    window.console.info = filteredLog;
+    window.console.warn = filteredLog;
+    window.console.debug = filteredLog;
+    window.debug = window.debug || {};
+}
